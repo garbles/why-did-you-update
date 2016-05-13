113d8c7855362c147d47b1c44da0205b6cca682c
Gabe Scholz <gabe@brewhouse.io>
2016-05-12 19:17:39 -0700

    Whoops. Add code back to demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -3,17 +3,13 @@ import {render} from 'react-dom'
 
 import whyDidYouUpdate from '../../src'
 
+whyDidYouUpdate(React)
+
 class ClassDemo extends React.Component {
   render () {
     return <div />
   }
 }
-
-const FactoryDemo = React.createClass({
-  render () {
-    return <div />
-  }
-})
 
 render(<ClassDemo a={1} b={{c: {d: 4}}} e={function something () {}} f={1} />, document.querySelector('#demo'))
 render(<ClassDemo a={1} b={{c: {d: 4}}} e={function something () {}} f={2} />, document.querySelector('#demo'))
