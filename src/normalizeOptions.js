418d589480e875897cccb3d1711cda8b0f86d42b
Gabe Scholz <gabe@brewhouse.io>
2016-05-14 10:38:21 -0700

    Allow an array to be used as an argument for include/exclude

--- src/normalizeOptions.js
+++ src/normalizeOptions.js
@@ -1,20 +1,38 @@
+import _isArray from 'lodash/isArray'
 import _isString from 'lodash/isString'
 import _isRegExp from 'lodash/isRegExp'
 
 export const normalizeOptions = (opts = {}) => {
   let {include, exclude} = opts
 
-  if (_isString(include)) {
-    include = new RegExp(`^${include}$`)
-  } else if (!_isRegExp(include)) {
-    include = /./
+  if (!_isArray(include)) {
+    include = [include]
   }
 
-  if (_isString(exclude)) {
-    exclude = new RegExp(`^${exclude}$`)
-  } else if (!_isRegExp(exclude)) {
-    exclude = /[^a-zA-Z0-9]/
+  if (!_isArray(exclude)) {
+    exclude = [exclude]
   }
+
+  include = include.map(i => {
+    if (_isString(i)) {
+      return new RegExp(`^${i}$`)
+    } else if (!_isRegExp(i)) {
+      return /./
+    }
+
+    return i
+  })
+
+  exclude = exclude.map(e => {
+    if (_isString(e)) {
+      return new RegExp(`^${e}$`)
+    } else if (!_isRegExp(e)) {
+      return /[^a-zA-Z0-9]/
+    }
+
+    return e
+  })
+
 
   return {...opts, include, exclude}
 }
