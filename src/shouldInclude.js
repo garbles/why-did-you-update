be6865b28a569cf484a1c84eaa16b34cbf90a807
Gabe Scholz <gabe@brewhouse.io>
2016-05-14 10:38:21 -0700

    Allow an array to be used as an argument for include/exclude

--- src/shouldInclude.js
+++ src/shouldInclude.js
@@ -1,8 +1,9 @@
+import _some from 'lodash/some'
 import _isFunction from 'lodash/isFunction'
 
 export const shouldInclude = (displayName, {include, exclude}) => {
-  let isIncluded = include.test(displayName)
-  let isExcluded = exclude.test(displayName)
+  let isIncluded = _some(include, r => r.test(displayName))
+  let isExcluded = _some(exclude, r => r.test(displayName))
 
   return isIncluded && !isExcluded
 }
