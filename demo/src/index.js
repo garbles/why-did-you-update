5756f11cac7b1de975955c76c724c2564229e9d9
Mofe Lee <mofe@me.com>
2016-05-15 19:38:27 +0800

    format file

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {render} from 'react-dom'
-import {Record}from 'immutable'
+import {Record} from 'immutable'
 
 const TestRecord = Record({h: 1})
 
@@ -15,4 +15,4 @@ class ClassDemo extends React.Component {
 }
 
 render(<ClassDemo a={1} b={{c: {d: 4}}} e={function something () {}} f={1} g={TestRecord({h: 2})} />, document.querySelector('#demo'))
-render(<ClassDemo a={1} b={{c: {d: 4}}} e={function something () {}} f={2} g={TestRecord({h: 2})}/>, document.querySelector('#demo'))
+render(<ClassDemo a={1} b={{c: {d: 4}}} e={function something () {}} f={2} g={TestRecord({h: 2})} />, document.querySelector('#demo'))
