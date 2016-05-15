4a09159016b4a1f58a4ac5e30f6f42f01e02e61d
Mofe Lee <mofe@me.com>
2016-05-15 19:19:37 +0800

    add immutable demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import {render} from 'react-dom'
+import {Record}from 'immutable'
+
+const TestRecord = Record({h: 1})
 
 import whyDidYouUpdate from '../../src'
 
@@ -11,5 +14,5 @@ class ClassDemo extends React.Component {
   }
 }
 
-render(<ClassDemo a={1} b={{c: {d: 4}}} e={function something () {}} f={1} />, document.querySelector('#demo'))
-render(<ClassDemo a={1} b={{c: {d: 4}}} e={function something () {}} f={2} />, document.querySelector('#demo'))
+render(<ClassDemo a={1} b={{c: {d: 4}}} e={function something () {}} f={1} g={TestRecord({h: 2})} />, document.querySelector('#demo'))
+render(<ClassDemo a={1} b={{c: {d: 4}}} e={function something () {}} f={2} g={TestRecord({h: 2})}/>, document.querySelector('#demo'))
