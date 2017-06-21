b56d23fd42ef8932dd81647db9a9b37e6d4560ee
Jason Kurian <jasonk@nulogy.com>
2017-06-21 17:15:24 -0400

    fix: remove createClass override and use create-react-class package

--- src/index.js
+++ src/index.js
@@ -33,30 +33,12 @@ function createComponentDidUpdate (opts) {
 
 export const whyDidYouUpdate = (React, opts = {}) => {
   const _componentDidUpdate = React.Component.prototype.componentDidUpdate
-  const _createClass = React.createClass
   opts = normalizeOptions(opts)
 
   React.Component.prototype.componentDidUpdate = createComponentDidUpdate(opts)
 
-  if (_createClass) {
-    React.createClass = function createClass (obj) {
-      if (!obj.mixins) {
-        obj.mixins = []
-      }
-
-      const Mixin = {
-        componentDidUpdate: createComponentDidUpdate(opts)
-      }
-
-      obj.mixins = [Mixin].concat(obj.mixins)
-
-      return _createClass.call(React, obj)
-    }
-  }
-
   React.__WHY_DID_YOU_UPDATE_RESTORE_FN__ = () => {
     React.Component.prototype.componentDidUpdate = _componentDidUpdate
-    React.createClass = _createClass
     delete React.__WHY_DID_YOU_UPDATE_RESTORE_FN__
   }
 
